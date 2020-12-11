c669b453a564aa5bed0363feb7343079e75b4165
Daniel Micay <danielmicay@gmail.com>
2020-12-11 15:38:41 -0500

    remove UpdateRecoveryActivity

--- src/org/lineageos/setupwizard/util/SetupWizardUtils.java
+++ src/org/lineageos/setupwizard/util/SetupWizardUtils.java
@@ -61,7 +61,6 @@ import org.lineageos.setupwizard.BiometricActivity;
 import org.lineageos.setupwizard.MobileDataActivity;
 import org.lineageos.setupwizard.SetupWizardApp;
 import org.lineageos.setupwizard.SimMissingActivity;
-import org.lineageos.setupwizard.UpdateRecoveryActivity;
 import org.lineageos.setupwizard.WifiSetupActivity;
 import org.lineageos.setupwizard.wizardmanager.WizardManager;
 
@@ -80,8 +79,6 @@ public class SetupWizardUtils {
     private static final String GMS_TV_SUW_PACKAGE = "com.google.android.tungsten.setupwraith";
     private static final String UPDATER_PACKAGE = "org.lineageos.updater";
 
-    private static final String UPDATE_RECOVERY_EXEC = "/vendor/bin/install-recovery.sh";
-    private static final String CONFIG_HIDE_RECOVERY_UPDATE = "config_hideRecoveryUpdate";
     private static final String PROP_BUILD_DATE = "ro.build.date.utc";
 
     private SetupWizardUtils(){}
@@ -122,23 +119,6 @@ public class SetupWizardUtils {
     public static boolean hasTelephony(Context context) {
         PackageManager packageManager = context.getPackageManager();
         return packageManager.hasSystemFeature(PackageManager.FEATURE_TELEPHONY);
-    }
-
-    public static boolean hasRecoveryUpdater(Context context) {
-        boolean fileExists = new File(UPDATE_RECOVERY_EXEC).exists();
-        if (!fileExists) {
-            return false;
-        }
-
-        boolean featureHidden = false;
-        try {
-            PackageManager pm = context.getPackageManager();
-            Resources updaterResources = pm.getResourcesForApplication(UPDATER_PACKAGE);
-            int res = updaterResources.getIdentifier(
-                    CONFIG_HIDE_RECOVERY_UPDATE, "bool", UPDATER_PACKAGE);
-            featureHidden = updaterResources.getBoolean(res);
-        } catch (PackageManager.NameNotFoundException | Resources.NotFoundException ignored) { }
-        return !featureHidden;
     }
 
     public static boolean isMultiSimDevice(Context context) {
